294186ece6279b07a0dbd1ab4ef61ccaad079a77
tfkhdyt <tfkhdyt@pm.me>
2022-10-08 17:16:27 +0700

    feat(dasar): add operator perbandingan

---
+++ belajar_dart_dasar/bin/operator_perbandingan.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('Taufik' == 'Taufik');
+  print('Taufik' != 'Taufik');
+}
